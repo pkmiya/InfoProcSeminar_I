2605ac95600cc992ca25109e3bfcd613a41f1bf3
__pkmiya <xbzd.cnlv@icloud.com>
2021-07-15 13:52:29 +0900

    Add files via upload

--- Assignment_4/client.py
+++ Assignment_4/client.py
@@ -1,3 +1,80 @@
+import socket
+from sense_emu import SenseHat
+
+def main():
+    sense = SenseHat()
+    sense.clear()
+    mylist = []
+    j = 0
+    RGB = []
+
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.connect(("127.0.0.1", 50007))
+    print("Connected.")
+
+    while True:
+        data = input("Client (q: quit, y: recv data) > ")
+        soc.send(data.encode())
+        if data == "q":
+            soc.close()
+            break
+        data = (soc.recv(1024)).decode()
+        print("cServer > ", data)
+        if(data[0] >= "0" or data[0] <= "9"):
+            datastr = data.split(",")
+            for i in datastr:
+                mylist[j] = float(datastr[j])
+                j += 1
+            j = 0
+            THI = 0.81 * mylist[0] + 0.01 * mylist[1] * (0.99 * mylist[0] - 14.3) + 46.3    # 45-110
+            if(THI > 60):
+                x = 255*(THI-45)/60
+                RGB = [x, 0, 255-x]
+            elif(THI > 75):
+                RGB = [0, 255, 0]
+            else:
+                x = 255*(THI-75)/110
+                RGB = [x, 255-x, 0]
+            sense.clear(RGB[0], RGB[1], RGB[2])
+        
+        # calc THI here
+        # display light accd. to THI
+
+    print("Finished.")
+
+if __name__ == "__main__":
+    main()
+
+# THI = 0.81 * t + 0.01 * H * (0.99 * t - 14.3) + 46.3
+#    - 60   :   blue    0  , 0  , 255
+# 60 - 75   :   green   0  , 255, 0
+# 75 -      :   red     255, 0  , 0
+
+"""
+# Real machine: from sense_hat import SenseHat
+# Emulator:     from sense_emu import SenseHat
+
+from sense_emu import SenseHat
+sense = SenseHat()
+import time
+
+sense.clear()
+while True:
+    for x in range(0, 255):
+        sense.clear(255, x, 0) #red to yellow
+    for x in range(0, 255):
+        sense.clear(255-x, 255, 0) #yellow to green
+    for x in range(0, 255):
+        sense.clear(0, 255, x) #green to sky-blue
+    for x in range(0, 255):
+        sense.clear(0, 255-x, 255) #sky-blue to blue
+    for x in range(0, 255):
+        sense.clear(x, 0, 255) #blue to purple
+    for x in range(0, 255):
+        sense.clear(255, 0, 255-x) #purple to red
+"""
+
+"""
 from sense_emu import SenseHat
 import socket
 import time
@@ -25,3 +102,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""
--- Assignment_4/server.py
+++ Assignment_4/server.py
@@ -1,9 +1,14 @@
 # coding: UTF-8
+import datetime
 import socket
+from sense_emu import SenseHat
 
 def main():
-    send_data = "Send your data"
+    path = "data.csv"
     
+    sense = SenseHat()
+    sense.clear()
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("", 50007))
     s.listen(1)
@@ -11,15 +16,25 @@ def main():
     print("Connected by " + str(addr))
 
     while True:
-        # data = input("Server > ")
-        data = send_data 
-        print("data = " + data)
-        soc.send(data.encode())
         data = (soc.recv(1024)).decode()
         print("Client > " , data)
         if data == "q":
+            # quit
             soc.close()
             break
+        elif data == "y":
+            # send data (temp, humid, press)
+            t = str(sense.get_temperature())
+            h = str(sense.get_humidity())
+            p = str(sense.get_pressure())
+            data = t + "," + h + "," + p
+            with open(path, mode="a") as f:
+                f.write(datetime.datetime.now() + "," + data + "\n")
+        else:
+            # normal messaging
+            data = input("Server >")
+        print("data = " + data)
+        soc.send(data.encode())
     print("Finished.")
 
 if __name__ == "__main__":
